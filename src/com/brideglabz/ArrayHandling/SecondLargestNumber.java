d5659bf212d78f6b6cc2df5a03b3f7040608e700
usha-12 <usha12ahirwar@gmail.com>
2023-01-09 14:30:10 +0530

    find 2nd Largest number in an array

---
+++ src/com/brideglabz/ArrayHandling/SecondLargestNumber.java
@@ -0,0 +1,24 @@
+package com.brideglabz.ArrayHandling;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondLargestNumber {
+    public static void secondLargestNumber(){
+        int array[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array of Elements :");
+        for (int i =0; i<array.length; i++){
+            array[i] = sc.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println("Second Largest Element in array is :" +array[array.length-2]);
+        System.out.println("First Largest Element in array is :" +array[array.length-1]);
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the  2nd Largest Number program");
+        secondLargestNumber();
+    }
+}
+//10) Find 2nd Largest Number in an Array
