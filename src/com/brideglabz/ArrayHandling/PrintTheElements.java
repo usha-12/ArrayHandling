2e0cecc749343a2a5e91813e2d29b57022f089a7
usha-12 <usha12ahirwar@gmail.com>
2023-01-09 14:02:22 +0530

    Print the elements of an array

--- src/com/brideglabz/ArrayHandling/PrintTheElements.java
+++ src/com/brideglabz/ArrayHandling/PrintTheElements.java
@@ -1,2 +1,20 @@
-package com.brideglabz.ArrayHandling;public class PrintTheElements {
+package com.brideglabz.ArrayHandling;
+
+import java.util.Scanner;
+
+public class PrintTheElements {
+    public static void printElement(){
+        Scanner sc = new Scanner(System.in);
+        int array = sc.nextInt();
+        int [] array1 = new int[array];
+        for (int i = 0; i< array1.length; i++){
+            array1[i] = i*10;
+            System.out.println(array1[i]);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("print the elements of an array");
+        printElement();
+    }
 }
+//1) Java Program to print the elements of an array
